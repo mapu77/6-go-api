1a03f2ad6c4e854510cc87508ef182582e2ddc7d
Eduard Maura <edu.maura.puig@gmail.com>
2017-12-17 18:26:34 +0100

    Extract port for production environment

--- main/main.go
+++ main/main.go
@@ -4,11 +4,16 @@ import (
 	"log"
 	"net/http"
 	"github.com/mapu77/AD-Labs/6-go-api/routes"
+	"os"
 )
 
 func main() {
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		port = "8080"
+	}
 
 	router := routes.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":80", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
