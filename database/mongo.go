49906950c15dc970f221aa9ade11b7285318c04c
Eduard Maura <edu.maura.puig@gmail.com>
2017-12-18 17:38:04 +0100

    Extract MONGODB name to var env

--- database/mongo.go
+++ database/mongo.go
@@ -12,16 +12,16 @@ func GetMongoDBSession() (*mgo.Session, string, error) {
 	mongoUri, exists := os.LookupEnv("MONGODB_URI")
 	if !exists {
 		// Assume api is connecting to a locally mongo db
-		mongoUri = "localhost:27017/ad-travel-agency"
+		mongoUri = "localhost:27017"
 	}
 	session, err := mgo.Dial(mongoUri)
 	if err != nil {
 		log.Fatal(err)
 	}
-	dbNames, err := session.DatabaseNames()
-	if err != nil {
-		log.Fatal(err)
-	}
 	session.SetMode(mgo.Monotonic, true)
-	return session, dbNames[0], err
+	dbName, exists := os.LookupEnv("MONGODB_NAME")
+	if !exists {
+		dbName = "ad-travel-agency"
+	}
+	return session, dbName, err
 }
